840b186065a53ab71ddec6e91c6588cff98724ab
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-09-19 21:07:57 -0600

    Completed Week 01 Code

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
+
 public static class Arrays
 {
     /// <summary>
@@ -8,12 +10,13 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        double[] multiples = new double[length]; //creating an empty array with a length of the requested number of 
+        for (int i = 1; i<=length; i++) { // we're going to start with a multiple of 1; by adding 1 to i each time and comparing it to the requested number of multiples we'll restrict the number of results to that given number.
+            double multiple = number * i; // formula to get multiples (Multiple = Number*integer)
+            multiples[(i-1)] = multiple; // we'll use our i to establish the index of where it needs to be placed in the array, minus 1 because indexes start at 0. 
+        }
+        return multiples;
     }
 
     /// <summary>
@@ -23,11 +26,24 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
+    /// 
+
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // we need a formula that will take a specified number and rotate a given list by that amount
+        // we'll need to use the .count() method to get the number 
+        
+        int rotation = data.Count - amount;
+        for(int i = 0; i<rotation; i++) {
+            data.Add(data[i]);   
+        }
+        for (int i = 0; i<rotation; i++) {
+            data.RemoveAt(0);
+        }
     }
 }
