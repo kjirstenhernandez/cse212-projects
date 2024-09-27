ccbf9bf15ab641953a703f60f249c2eb5952ddf1
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-09-27 01:14:43 -0600

    Completed TestPrioirtyQueue_1

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -9,6 +9,8 @@
     /// </summary>
     /// <param name="value">The value</param>
     /// <param name="priority">The priority</param>
+    
+    
     public void Enqueue(string value, int priority)
     {
         var newNode = new PriorityItem(value, priority);
@@ -24,14 +26,15 @@
 
         // Find the index of the item with the highest priority to remove
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
+        for (int index = 0; index < _queue.Count; index++)
         {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
-                highPriorityIndex = index;
+            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority) {
+                highPriorityIndex = index;}
         }
 
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex);
         return value;
     }
 
@@ -39,7 +42,13 @@
     {
         return $"[{string.Join(", ", _queue)}]";
     }
+
+    public int queueCount(){
+        int value = _queue.Count;
+        return value;
+    }
 }
+
 
 internal class PriorityItem
 {
--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,13 +6,32 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Items with varying priority are added into the queue, 
+    //and the most important are completed first.
+    // Expected Result: high2, high1, mid2, mid1, low2, low1
+    // Defect(s) Found: Dequeue returned the value but did not remove it from the queue; index in Dequeue 
+    // started at 1 instead of 0; 'if' statement didn't include curly braces for 'then'; dequeue for loop wasn't
+    // checking the full list for priority items. 
     public void TestPriorityQueue_1()
     {
+        string[] expectedResult = {"high2", "high1","mid2", "mid1", "low2", "low1"};
+
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("low1", 1);
+        priorityQueue.Enqueue("high1", 5);
+        priorityQueue.Enqueue("mid1", 3);
+        priorityQueue.Enqueue("low2", 2);
+        priorityQueue.Enqueue("high2", 6);
+        priorityQueue.Enqueue("mid2", 4);
+        
+        int i = 0;
+        int count = priorityQueue.queueCount();
+        for(; i < count; i++) {
+            var value = priorityQueue.Dequeue();
+            Assert.AreEqual(expectedResult[i], value);
+        }
+        
+    
     }
 
     [TestMethod]
@@ -26,4 +45,4 @@ public class PriorityQueueTests
     }
 
     // Add more test cases as needed below.
-}
+} 
